f648a3c150aa9a8823fe2b67d3e9ad7db3417a71
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2020-04-09 22:45:42 -0500

    add spectre.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
+import * as spectre from 'spectre.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
